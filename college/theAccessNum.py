c3733af09a7bd616484c228ea17195c320a95c21
“beforeuwait” <forme.wjw@aliyun.com>
2016-12-19 11:09:37 +0800

    各学校各省录取人数

--- college/theAccessNum.py
+++ college/theAccessNum.py
@@ -43,17 +43,17 @@ class theAccessNum():
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24", \
             "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
         ]
-
-        self.headers = {'Host': 'www.gaokao.com',
-                        'User-Agent': random.choice(self.user_agent_list)
-                        }
         self.session = requests.session()
         self.province = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
                          24, 25, 26, 27, 28, 29, 30, 31, 33, 38, 39]
         self.type = [1, 2, 3, 4, 8, 9]
     def setupsession(self):
         try:
-            r = self.session.get('http://www.gaokao.com', headers=self.headers, timeout= 10)
+            headers = {
+                'Host': 'www.gaokao.com',
+                'User-Agent': random.choice(self.user_agent_list),
+            }
+            r = self.session.get('http://www.gaokao.com', headers=headers)
             cookies = r.cookies
             self.session.cookies.update(cookies)
             # 建立mysql链接
@@ -78,54 +78,67 @@ class theAccessNum():
         for each_college in range(2666):
             for each_province in self.province:
                 for each_type in self.type:
-                    url = 'http://college.gaokao.com/school/tinfo'+ str(each_college) \
+                    url = 'http://college.gaokao.com/school/tinfo/'+ str(each_college + 1) \
                           + '/result/' + str(each_province) + '/' + str(each_type) + '/'
                     self.getData(url)
         print '抓取完毕'
         self.conn.close()
+        print '失效的链接有'
+        for wrong_link in urls:
+            print wrong_link
 
     def getData(self, url):
         try:
-            selector = etree.HTML(self.session.get(url, headers=self.headers, timeout= 10).content)
-            content = selector.xpath('//div[@id="wrapper"]/div[@class="cont_l in"]')
+            headers = {
+                'Host': 'college.gaokao.com',
+                'User-Agent': random.choice(self.user_agent_list),
+            }
+            selector = etree.HTML(self.session.get(url, headers=headers, timeout=5).content)
             '''
             将 学校+地区+考生类别 同 数据分开。遇到空数据，仍录入 学校+地区+考生类别的 数据.
             '''
-            school_name = content.xpath('p[1]/font[1]/text()')[0]
-            area = content.xpath('p[1]/font[2]/text()')[0]
-            s_type = content.xpath('p[1]/font[3]/text()')[0]
-            if content.xpath('div[@class="ts"]'):
-            #无数据，返回空的插入
-                SQL = 'insert into access_num(学校名称,地区,考生类别)values(\'%s\',\'%s\',\'%s\')'%(school_name, area, s_type)
+            school_name = selector.xpath('//div[@class="cont_l in"]/p/font[1]/text()')[0]
+            area = selector.xpath('//div[@class="cont_l in"]/p/font[2]/text()')[0]
+            s_type = selector.xpath('//div[@class="cont_l in"]/p/font[3]/text()')[0]
+
+            if selector.xpath('//div[@class="cont_l in"]/div[@class="ts"]'):
+                # 无数据，返回空的插入
+                SQL = 'insert into access_num(学校名称,地区,考生类别)values(\'%s\',\'%s\',\'%s\')' % (school_name, area, s_type)
                 self.cur.execute(SQL)
                 self.conn.commit()
 
-            elif content.xpath('div[@id="pointbyarea"]/tr'):
-            #有数据，采集
-                for each_info in content.xpath('div[@id="pointbyarea"]/tr'):
+            elif selector.xpath('//div[@class="cont_l in"]/div[@id="pointbyarea"]/table/tr'):
+                # 有数据，采集
+                for each_info in selector.xpath('//div[@class="cont_l in"]/div[@id="pointbyarea"]/table/tr'):
                     if each_info.xpath('td[1]/text()'):
                         year = each_info.xpath('td[1]/text()')[0]
-                    else: year = ''
+                    else:
+                        year = ''
                     if each_info.xpath('td[2]/text()'):
                         min = each_info.xpath('td[2]/text()')[0]
-                    else: min = ''
+                    else:
+                        min = ''
                     if each_info.xpath('td[3]/text()'):
                         max = each_info.xpath('td[3]/text()')[0]
-                    else: max = ''
+                    else:
+                        max = ''
                     if each_info.xpath('td[4]/text()'):
                         ave = each_info.xpath('td[4]/text()')[0]
-                    else: ave = ''
+                    else:
+                        ave = ''
                     if each_info.xpath('td[5]/text()'):
                         num = each_info.xpath('td[5]/text()')[0]
-                    else: num = ''
+                    else:
+                        num = ''
                     if each_info.xpath('td[6]/text()'):
                         admission_type = each_info.xpath('td[6]/text()')[0]
-                    else: admission_type = ''
+                    else:
+                        admission_type = ''
 
                     if year != '':
                         SQL = 'insert into access_num(学校名称,地区,考生类别,年份,最低,最高,平均,录取人数,录取批次)' \
-                          'value(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')'\
-                          %(school_name, area, s_type, year, min, max, ave, num, admission_type)
+                              'value(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')' \
+                              % (school_name, area, s_type, year, min, max, ave, num, admission_type)
                         self.cur.execute(SQL)
                         self.conn.commit()
 
