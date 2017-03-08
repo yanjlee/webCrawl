93fee6b6c9e7ff8c073f790eabd54e73b9e3de51
beforeuwait <forme.wjw@aliyun.com>
2017-03-08 17:45:22 +0800

    景点爬虫

---
+++ OTA/JingDian/getDataFromCtrip.py
@@ -0,0 +1,176 @@
+# coding=utf8
+
+'''
+Info:
+- author    : wangjiawei
+- email     : wangjw@daqsoft.com.cn
+- date      : 2017,03,08
+Update:
+- name      :
+- email     :
+- date      :
+'''
+
+
+import requests
+import json
+import random
+import time
+import re
+
+
+from lxml import etree
+from JingDian import USER_AGENT_LIST
+
+#配置
+class CtripSetting():
+    ua = random.choice(USER_AGENT_LIST)
+    host = 'piao.ctrip.com'
+    headers = {
+        'User-Agent': ua,
+        'Host': host,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive'
+    }
+
+#从list.txt列表里提取数据,放入list里
+class loadInList():
+    list_a = open('list.txt', 'r').readlines()
+    #去'\n'
+    list_b = []
+    for i in list_a:
+        list_b.append(i.replace('\n', ''))
+
+#建立session,不需要登录就没有登录模块
+class setUpSession(CtripSetting):
+    session = requests.session()
+    session.headers.update(CtripSetting.headers)
+    def get_json(params):
+        url = 'http://piao.ctrip.com/thingstodo-booking-bookingwebsite/api/AutoComplete'
+        params = {
+            'keyWords': params,
+            'pageIndex': 1,
+            'count': 15
+        }
+        return setUpSession.session.get(url, params=params).text
+
+    def get_html(url):
+        new_url = 'http://piao.ctrip.com' + url
+        # return setUpSession.session.get(new_url).text
+        return setUpSession.session.get('http://piao.ctrip.com/dest/t13720.html').text
+
+#先搜索一遍,存放数据,再进行抓取
+class getDataFromJson(setUpSession):
+    def __init__(self, column_spilt='\u0001'):
+        self.column_spilt = column_spilt
+    #建立session
+    def makeSession(self):
+        js_list = loadInList.list_b
+        for each in js_list:
+            response = setUpSession.get_json(each)
+            self.dealJson(response)
+            time.sleep(3)
+            break
+    #将数据放入文档
+    def dealJson(self, content):
+        if json.loads(content)['SearchList']:
+            jdKw = json.loads(content)['SearchList'][0]['Name']
+            jsDict = json.loads(content)['SearchList'][0]['Url']
+            self.save(jdKw, jsDict)
+    def save(self, name, url):
+        text = '%s\u0001%s\u0001\n' %(name, url)
+        with open('Json.txt', 'a', encoding='utf-8') as f:
+            f.writelines(text)
+
+#从json中提取url开始抓取景点数据
+class getDataFromHtml(setUpSession):
+    def __init__(self, column_split='\u0001', jd_name=None, jd_star=None, jd_loc=None, jd_time=None, jd_tese=None,
+                 jd_jj=None, jd_jt=None, jd_point=None):
+        self.column_split = column_split
+        self.jd_name = jd_name
+        self.jd_star = jd_star
+        self.jd_loc = jd_loc
+        self.jd_time = jd_time
+        self.jd_tese = jd_tese
+        self.jd_jj = jd_jj
+        self.jd_jt = jd_jt
+        self.jd_point = jd_point
+        self.error = []
+    def getInfoFromText(self):
+        text = open('Json.txt', 'r', encoding='utf-8').readlines()
+        for eachline in text:
+            name = eachline.split(self.column_split)[0]
+            url = eachline.split(self.column_split)[1]
+            print(name, url)
+            self.makeSession(name, url)
+            time.sleep(3)
+
+    def makeSession(self, name, url):
+        response = setUpSession.get_html(url)
+        self.spyder(name, response, url)
+
+    def spyder(self, name, response, url):
+        try:
+            selector = etree.HTML(response)
+            jd_name = selector.xpath('//div[@class="media-right"]/h2/text()')[0]
+            if selector.xpath('//div[@class="media-right"]/span/text()'):
+                jd_star = selector.xpath('//div[@class="media-right"]/span')[0].xpath('string(.)')
+            if selector.xpath('//div[@class="media-right"]/ul/li[1]/span/text()'):
+                jd_loc = selector.xpath('//div[@class="media-right"]/ul/li[1]/span/text()')[0].\
+                    replace('\r\n', '').replace(' ', '')
+            if selector.xpath('//div[@class="media-right"]/ul/li[2]/span/text()'):
+                jd_time = selector.xpath('//div[@class="media-right"]/ul/li[2]/span/text()')[0]
+            #景点简介
+            if selector.xpath('//div[@id="J-Jdjj"]/div[2]/div/p/text()'):
+                jd_tese = selector.xpath('//div[@id="J-Jdjj"]/div[2]/ul')[0].xpath('string(.)').\
+                    replace('\r\n', '').replace(' ', '')
+                jd_jj = selector.xpath('//div[@id="J-Jdjj"]/div[2]/div')[0].xpath('string(.)')
+            #交通指南
+            if selector.xpath('//div[@id="J-Jtzn"]/div[@class="feature-traffic"]/text()'):
+                jd_jt = selector.xpath('//div[@id="J-Jtzn"]/div[@class="feature-traffic"]')[0].xpath('string(.)').\
+                    replace('\r\n', '').replace(' ', '')
+            self.get_position(response)
+            self.save()
+        except Exception as e:
+            print(e)
+            self.error.append(name + '\u0001' + url)
+    def get_position(self, response):
+        try:
+            text = re.findall('type="text/javascript" charset="utf-8"(.*?)minPrice:', response, re.S)[0]
+            self.jd_point = re.findall('position: \'(.*?)\',', text, re.S)[0]
+            return self.jd_point
+        except Exception as e:
+            print(e)
+
+    def save(self):
+        text = self.jd_name + self.column_split + self.jd_star + self.column_split + self.jd_loc + self.jd_jj +\
+               self.column_split + self.jd_time + self.column_split + self.jd_tese + self.column_split + self.jd_jt +\
+                self.jd_point + self.column_split
+        with open('data2.txt', 'a', encoding='utf-8') as f:
+            print(self.jd_name)
+            f.writelines(text + '\n')
+if __name__ == '__main__':
+    CHECK_CHOICE = True
+    while(CHECK_CHOICE):
+        try:
+            print('携程景点抓取爬虫,请选择抓取项目:')
+            print('\t1.搜索数据保存json')
+            print('\t2.抓取景点信息')
+            choice = input('请输入: ')
+            if choice not in [1, 2]:
+                raise Exception('请重新选择编号.')
+                CHECK_CHOICE = False
+        except Exception as e:
+            print(e)
+        if choice is '1':
+            gdf = getDataFromJson()
+            print('正在启动...')
+            gdf.makeSession()
+            CHECK_CHOICE = False
+        elif choice is '2':
+            gdh = getDataFromHtml()
+            gdh.getInfoFromText()
+            CHECK_CHOICE = False
--- OTA/JingDian/getDataFromQunar.py
+++ OTA/JingDian/getDataFromQunar.py
@@ -1,4 +1,5 @@
 # coding=utf8
+
 '''
 Info:
 - author    : wangjiawei
@@ -53,11 +54,15 @@ class setUpSession(setting):
             'key': params
         }
         return setUpSession.session.get(url, params=params).text
-
+    def get_url(keyword):
+        url = 'http://piao.qunar.com/ticket/list.htm'
+        params = {
+            'keyword': keyword
+        }
+        return setUpSession.session.get(url, params=params).text
     def get_page(num):
         url = 'http://piao.qunar.com/ticket/detail_' + num + '.html'
         return setUpSession.session.get(url).text
-
 
 #录入json中的数据
 class dealWithJson(setUpSession):
@@ -65,8 +70,8 @@ class dealWithJson(setUpSession):
         self.column_split = column_split
     #建立session
     def makeSession(self):
-        jd_list = loadInList.list_b
-        for each in jd_list:
+        json_list = loadInList.list_b
+        for each in json_list:
             #既然是继承,这里就要重写
             response = setUpSession.get_json(each)
             self.dealJson(response)
@@ -110,22 +115,38 @@ class getJingDianData(setUpSession):
         self.jd_ruyuan = jd_ruyuan
         self.jd_tips = jd_tips
         self.jd_traffic = jd_traffic
+        self.item = []
+    #获取搜索列表
+    def get_List(self):
+        list = loadInList.list_b
+        self.makeSession_url(list)
+    def makeSession_url(self, content):
+        for each in content:
+            response = setUpSession.get_url(each)
+            selector = etree.HTML(response)
+            if selector.xpath('//a[@class="sight_item_do"][1]/@href'):
+                url = selector.xpath('//a[@class="sight_item_do"][1]/@href')[0]
+                self.num = re.findall('/ticket/detail_(.*?).html', url, re.S)[0]
+                self.makeSession(self.num, each)
+                time.sleep(1)
+            else:
+                continue
+        print('抓取完毕,失效的有')
+        for i in self.item:
+            with open('exdata.txt', 'a') as f:
+                f.writelines(i + '\n')
     def getNum(self):
         data_list = open('search_list.txt', 'r', encoding='utf8').readlines()
-        # print(data_list)
         text = []
         for each in data_list:
-            self.name = each.split('\u0001')[0]
-            self.loc = each.split('\u0001')[1]
             self.num = each.split('\u0001')[2]
-            self.type = each.split('\u0001')[3]
-            self.makeSession(self.name, self.loc, self.num, self.type)
+            self.makeSession(self.num, each)
             time.sleep(3)
 
-    def makeSession(self, name, loc, num, type):
+    def makeSession(self,num, item):
         response = setUpSession.get_page(num)
 
-        return self.spyder(response)
+        return self.spyder(response, item)
     #获取景点经纬度和电话
     def get_jingweiduAndTel(self, response):
         text = re.findall('window.context =(.*?)window.context', response, re.S)[0]
@@ -136,86 +157,99 @@ class getJingDianData(setUpSession):
         if re.findall('"phone": "(.*?)",', text, re.S):
             self.jd_Tel = re.findall('"phone": "(.*?)",', text, re.S)[0]
 
-    def spyder(self, response):
+    def spyder(self, response, item):
         selector = etree.HTML(response)
         #景区名字
-        self.jd_name = selector.xpath('//div[@class="mp-description-view"]/span[1]/text()')[0]
-        #景区等级
-        if selector.xpath('//div[@class="mp-description-view"]/span[2]'):
-            self.jd_star = selector.xpath('//div[@class="mp-description-view"]/span[2]/text()')[0]
-        #景区一句话描述
-        if selector.xpath('//div[@class="mp-description-onesentence"]/text()'):
-            self.jd_miaosu = selector.xpath('//div[@class="mp-description-onesentence"]/text()')[0]
-        #景区位置
-        if selector.xpath('//div[@class="mp-description-location"]/span[3]/text()'):
-            self.jd_loc = selector.xpath('//div[@class="mp-description-location"]/span[3]/text()')[0]
-        #景区网友评分
-        if selector.xpath('//div[@class="mp-description-comments"]/span[3]/span/text()'):
-            self.jd_pingfen = selector.xpath('//div[@class="mp-description-comments"]/span[3]/span/text()')[0]
-        #景区简介
-        if selector.xpath('//div[@class="mp-charact-intro"]/div/p/text()'):
-            self.jd_jianjie = selector.xpath('//div[@class="mp-charact-intro"]/div/p/text()')[0]
-        #景区开放时间
-        if selector.xpath('//div[@class="mp-charact-time"]/div/div[2]/p/text()'):
-            self.jd_shijian = selector.xpath('//div[@class="mp-charact-time"]/div/div[2]/p')\
-            [0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
-        #景区门票
-        if selector.xpath('//div[@class="mp-description-price"]/span/text()'):
-            self.jd_price = selector.xpath('//div[@class="mp-description-price"]/span')[0].xpath('string(.)').\
-                replace('\r\n', '').replace(' ','').replace('¥', '')
-        #景区特色
-        if selector.xpath('//div[@class="mp-charact-event"]'):
-            self.jd_tese = ''
-            self.jd_teseContent = ''
-            for each1 in selector.xpath('//div[@class="mp-charact-event"]'):
-                tese_name = each1.xpath('div/div[2]/h3/text()')[0]
-                self.jd_tese += tese_name + ','
-                # 特色说明
-                if each1.xpath('div/div[2]/p/text()'):
-                    self.jd_teseContent += each1.xpath('div/div[2]/p/text()')[0] + ';'
-                #景点照片
-                if each1.xpath('div/img'):
-                    img = each1.xpath('div/img/@src')[0]
-                    #保存图片
-                    # self.save_pic(img, tese_name)
-        #其余信息
-        if selector.xpath('//div[@class="mp-charact-littletips"][1]'):
-            self.jd_ruyuan = '入园公告 :'
-            ruyuan = selector.xpath('//div[@class="mp-charact-littletips"][1]/div/div[@class="mp-littletips-item"]')
-            for each2 in ruyuan:
-                s1 = each2.xpath('div[1]/text()')[0]
-                s2 = each2.xpath('div[2]')[0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
-                self.jd_ruyuan += s1 + ':' + s2
-        if selector.xpath('//div[@class="mp-charact-littletips"][2]'):
-            self.jd_tips = '小贴士 :'
-            tips = selector.xpath('//div[@class="mp-charact-littletips"][2]/div/div[@class="mp-littletips-item"]')
-            for each3 in tips:
-                d1 = each3.xpath('div[1]/text()')[0]
-                d2 = each3.xpath('div[2]')[0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
-                self.jd_tips += d1 + ':' + d2
-        #交通信息
-        if selector.xpath('//div[@class="mp-traffic-transfer"]/div'):
-            n = 1
-            self.jd_traffic = ''
-            while(True):
-                if selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n) + ']'):
-                    title = selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n) + ']/text()')[0]
-                    fangshi = selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n+1) + ']')[0]\
-                        .xpath('string(.)')
-                    self.jd_traffic += title + ': ' + fangshi + ';'
-                else:
-                    break
-                n += 2
-        self.get_jingweiduAndTel(response)
-        self.save_txt()
+        try:
+            self.jd_name = selector.xpath('//div[@class="mp-description-view"]/span[1]/text()')[0]
+            #景区等级
+            if selector.xpath('//div[@class="mp-description-view"]/span[2]'):
+                self.jd_star = selector.xpath('//div[@class="mp-description-view"]/span[2]/text()')[0]
+
+            #景区一句话描述
+            if selector.xpath('//div[@class="mp-description-onesentence"]/text()'):
+                self.jd_miaosu = selector.xpath('//div[@class="mp-description-onesentence"]/text()')[0]
+
+            #景区位置
+            if selector.xpath('//div[@class="mp-description-location"]/span[3]/text()'):
+                self.jd_loc = selector.xpath('//div[@class="mp-description-location"]/span[3]/text()')[0]
+
+            #景区网友评分
+            if selector.xpath('//div[@class="mp-description-comments"]/span[3]/span/text()'):
+                self.jd_pingfen = selector.xpath('//div[@class="mp-description-comments"]/span[3]/span/text()')[0]
+            #景区简介
+            if selector.xpath('//div[@class="mp-charact-intro"]/div/p/text()'):
+                self.jd_jianjie = selector.xpath('//div[@class="mp-charact-intro"]/div/p/text()')[0]
+            #景区开放时间
+            if selector.xpath('//div[@class="mp-charact-time"]/div/div[2]/p/text()'):
+                self.jd_shijian = selector.xpath('//div[@class="mp-charact-time"]/div/div[2]/p')\
+                [0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
+            #景区门票
+            if selector.xpath('//div[@class="mp-description-price"]/span/text()'):
+                self.jd_price = selector.xpath('//div[@class="mp-description-price"]/span')[0].xpath('string(.)').\
+                    replace('\r\n', '').replace(' ','').replace('¥', '')
+            #景区特色
+            if selector.xpath('//div[@class="mp-charact-event"]'):
+                self.jd_tese = ''
+                self.jd_teseContent = ''
+                for each1 in selector.xpath('//div[@class="mp-charact-event"]'):
+                    if each1.xpath('div/div[2]/h3/text()'):
+                        tese_name = each1.xpath('div/div[2]/h3/text()')[0]
+                        self.jd_tese += tese_name + ','
+                    # 特色说明
+                    if each1.xpath('div/div[2]/p/text()'):
+                        self.jd_teseContent += each1.xpath('div/div[2]/p/text()')[0] + ';'
+                    #景点照片
+                    if each1.xpath('div/img'):
+                        img = each1.xpath('div/img/@src')[0]
+                        #保存图片
+                        # self.save_pic(img, tese_name)
+
+            #其余信息
+            if selector.xpath('//div[@class="mp-charact-littletips"][1]'):
+                self.jd_ruyuan = '入园公告 :'
+                ruyuan = selector.xpath('//div[@class="mp-charact-littletips"][1]/div/div[@class="mp-littletips-item"]')
+                for each2 in ruyuan:
+                    s1 = each2.xpath('div[1]/text()')[0]
+                    s2 = each2.xpath('div[2]')[0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
+                    self.jd_ruyuan += s1 + ':' + s2
+            if selector.xpath('//div[@class="mp-charact-littletips"][2]'):
+                self.jd_tips = '小贴士 :'
+                tips = selector.xpath('//div[@class="mp-charact-littletips"][2]/div/div[@class="mp-littletips-item"]')
+                for each3 in tips:
+                    d1 = each3.xpath('div[1]/text()')[0]
+                    d2 = each3.xpath('div[2]')[0].xpath('string(.)').replace('\r\n', '').replace(' ', '')
+                    self.jd_tips += d1 + ':' + d2
+
+            #交通信息
+
+            if selector.xpath('//div[@class="mp-traffic-transfer"]/div'):
+                n = 1
+                self.jd_traffic = ''
+                while(True):
+                    if selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n) + ']'):
+                        title = selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n) + ']/text()')[0].\
+                            replace('\n', '').replace('\r', '').replace(' ', '')
+                        fangshi = selector.xpath('//div[@class="mp-traffic-transfer"]/div[' + str(n+1) + ']')[0]\
+                            .xpath('string(.)').replace('\n', '').replace('\r', '').replace(' ', '')
+                        self.jd_traffic += title + ': ' + fangshi + ';'
+                    else:
+                        break
+                    n += 2
+            self.get_jingweiduAndTel(response)
+            self.save_txt()
+        except Exception as e:
+            print(e)
+            self.item.append(item)
+
     def save_txt(self):
         text = str(self.jd_name) + str(self.column_split) + str(self.jd_star[0]) + str(self.column_split) +\
                str(self.jd_loc) + str(self.column_split) + str(self.jd_jianjie) + str(self.column_split) + \
                str(self.jd_shijian) + str(self.column_split) + str(self.jd_price) + str(self.column_split) + \
                str(self.jd_tese) + str(self.column_split) + str(self.jd_ruyuan) + str(self.column_split) + \
-               str(self.jd_tips) + str(self.jd_traffic) + str(self.column_split) + \
-               str(self.baidu_point) + str(self.column_split) + str(self.jd_Tel)
-
+               str(self.jd_tips)+ str(self.column_split) + str(self.jd_traffic) + str(self.column_split) + \
+               str(self.baidu_point) + str(self.column_split) + str(self.jd_Tel) + str(self.column_split)
+        print(self.jd_name)
         with open('data.txt', 'a', encoding='utf-8') as f:
             f.writelines(text+ '\n')
     def save_pic(self, url, name):
@@ -223,9 +257,24 @@ class getJingDianData(setUpSession):
             request.urlretrieve(url, filename=os.path.abspath('imgs') + '/' + self.jd_name + ',' + name + '.jpg')
         except :
             pass
+    def get_commit(self):
+        setUpSession.session.get_commit()
 if __name__ == '__main__':
-    # c = setUpSession()
-    # d = dealWithJson()
-    # d.makeSession()
-    g = getJingDianData()
-    g.getNum()
+    print('选择项目: \n\t1.搜索抓取全部\n\t2.收缩抓取一条')
+    INPUT_CHEKC = False
+    while(True):
+        iput = input('请选择: ')
+        if iput is '1':
+            dJson = dealWithJson()
+            dJson.makeSession()
+
+        elif iput is '2':
+            gJD = getJingDianData()
+            gJD.get_List()
+        else:
+            print('重新选择')
+            INPUT_CHEKC = True
+        if INPUT_CHEKC:
+            continue
+        else:
+            break
